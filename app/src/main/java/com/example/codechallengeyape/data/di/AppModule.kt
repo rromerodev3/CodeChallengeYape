f55f110e9b66ce304252a658c3e586eab3082076
Roger Romero <rromerodev@gmail.com>
2024-07-12 23:06:09 -0600

    hilt implemented

---
+++ app/src/main/java/com/example/codechallengeyape/data/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.example.codechallengeyape.data.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule
--- app/src/main/java/com/example/codechallengeyape/presentation/MainActivity.kt
+++ app/src/main/java/com/example/codechallengeyape/presentation/MainActivity.kt
@@ -1,12 +1,16 @@
-package com.example.codechallengeyape
+package com.example.codechallengeyape.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.codechallengeyape.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
